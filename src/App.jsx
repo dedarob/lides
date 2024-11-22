4c0c4d4eedbb380b66b6813c9df8edd89286541d
dedarob <euclidesroberto16@gmail.com>
2024-11-22 11:25:43 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      Content 
+      <Footer />
+    </>
+  )
+}
+
+export default App
